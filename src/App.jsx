eb237aed7bc2476042d196a18357d718c7db886f
Toan66 <congtoan060601@gmail.com>
2024-04-05 00:15:47 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-			<div className="shrink-0">
-			</div>
-			<div>
-				<div className="text-xl font-medium text-red-600">ChitChat</div>
-				<p className="text-slate-500">You have a new message!</p>
-			</div>
-			<Test/>
+		<div>
 			<Login/>
 		</div>
 	)
