b273662f826ccd9259ef2d53bd8d1b3856741823
Toan66 <congtoan060601@gmail.com>
2024-04-04 22:03:07 +0700

    smt

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 			<div className="shrink-0">
 			</div>
 			<div>
-				<div className="text-xl font-medium text-black">ChitChat</div>
+				<div className="text-xl font-medium text-red-600">ChitChat</div>
 				<p className="text-slate-500">You have a new message!</p>
 			</div>
 		</div>
---
+++ src/Test.jsx
@@ -0,0 +1,9 @@
+function Test(){
+    return(
+        <div className="">
+            {/* <div className="p-6 max-w-sm mx-auto bg-white rounded  }*/}
+        </div>
+    )
+}
+
+export default Test;
