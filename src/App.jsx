4cbb09d4303dadd71251047b39d8e2c49c9e66c8
Toan66 <congtoan060601@gmail.com>
2024-04-04 21:50:20 +0700

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline bg-green-300">Đã Commit</h1>
-		</>
+		<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+			<div className="shrink-0">
+			</div>
+			<div>
+				<div className="text-xl font-medium text-black">ChitChat</div>
+				<p className="text-slate-500">You have a new message!</p>
+			</div>
+		</div>
 	)
 }
 
