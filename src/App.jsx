8185f3bf1950700a56282ffe59b6b8c49b76fb17
Toan66 <congtoan060601@gmail.com>
2024-04-05 07:48:15 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Test from './Test'
 import Login from './Pages/Login'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 function App() {
 	const [count, setCount] = useState(0)
 
 	return (
-		<div>
+		<>
+			<Header/>
 			<Login/>
-		</div>
+			<Footer/>
+		</>
 	)
 }
 
