264b344585afe9c9999f78e04c6fc25f0acedde1
Toan66 <congtoan060601@gmail.com>
2024-04-06 23:22:07 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Test from './Test'
 import Login from './Pages/Login'
 import Layout from './Components/Layout'
 function App() {
--- src/Test.jsx
+++
@@ -1,9 +0,0 @@
-function Test(){
-    return(
-        <div className="rounded-xl m-5 p-2 border-2 border-red-200 text-primary">
-            Hello
-        </div>
-    )
-}
-
-export default Test;
