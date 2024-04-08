ac8fdb4ad0560cb7d12f86ed49a9c1c65f06190b
Cong Toan <congtoan060601@gmail.com>
2024-04-08 22:35:28 +0700

    somethings

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,13 @@ import Home from './Pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 	return (
-		<Router>
-			<Layout>
-				<Routes>
-					<Route path="/login" element={<Login />} />
-					<Route path="/" exact element={<Home />} />
-					{/* Thêm các Route khác tại đây */}
-				</Routes>
-			</Layout>
-		</Router>
+		<Layout>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/" exact element={<Home />} />
+			</Routes>
+		</Layout>
 	);
-
 }
 
 export default App
